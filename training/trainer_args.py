f54face283826029972e9c56de89454564742fd3
Felipe Perdomo <felipe.perdomo@idatha.com>
2026-03-28 22:11:15 -0300

    e-4, 25, batch 32, f1

--- training/trainer_args.py
+++ training/trainer_args.py
@@ -1,6 +1,6 @@
 from transformers import TrainingArguments
 
-def get_training_args(output_dir, lr=3e-4, epochs=25): #3e-5, 25 epochs
+def get_training_args(output_dir, lr=1e-4, epochs=25): #3e-5, 25 epochs
     return TrainingArguments(
         output_dir=output_dir,
         per_device_train_batch_size=32,
